5562a4fd4ac92728a7eb5d6371dc966326b17267
Brett <brett@brettfreeman.io>
2019-05-11 17:52:38 -0400

    Add file checking to uninstall script.

--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,12 @@ unlink ~/.vimrc
 unlink ~/.bashrc
 unlink ~/.tmux.conf
 
-mv ~/.vimrc.bak ~/.vimrc
-mv ~/.bashrc.bak ~/.bashrc
-mv ~/.tmux.conf.bak ~/.tmux.conf
+if [ -f ~/.vimrc.bak ]; then
+  mv ~/.vimrc.bak ~/.vimrc
+fi
+if [ -f ~/.bashrc.bak ]; then
+  mv ~/.bashrc.bak ~/.bashrc
+fi
+if [ -f ~/.tmux.conf.bak ]; then
+  mv ~/.tmux.conf.bak ~/.tmux.conf
+fi
