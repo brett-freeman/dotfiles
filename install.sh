3cbc256529fc1b79542704927aaab773ba4e9229
= <=>
2024-01-14 08:39:36 -0500

    Revamped install script.

--- install.sh
+++ install.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-set -e
+function ask() {
+    read -p "$1 (Y/n): " resp
+    if [ -z "$resp" ]; then
+        response_lc="y"
+    else
+        response_lc=$(echo "$resp" | tr '[:upper:]' '[:lower:]')
+    fi
 
-CWD=$(pwd)
+    [ "$response_lc" == "y" ]
+}
 
-if [ -f ~/.tmux.conf ]; then
-	mv ~/.tmux.conf ~/.tmux.conf.bak
+SH="${HOME}/.bashrc"
+ZSHRC="${HOME}/.zshrc"
+if [ -f "$ZSHRC" ]; then
+    SH=$ZSHRC
 fi
-if [ -f ~/.bashrc ]; then
-	mv ~/.bashrc ~/.bashrc.bak
+
+echo >> $SH
+
+echo "Do you want $SH to source: "
+for file in shell/*; do
+    if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        if ask "${filename}?"; then
+            echo "source $(realpath "$file")"  >> "$SH"
+        fi
+    fi
+done
+
+if ask "Do you want to install .tmux.conf?" ; then
+    ln -s $(realpath .tmux.conf) ~/.tmux.conf
 fi
 
-# Link new configs
-ln -s $CWD/.bashrc ~/.bashrc
-ln -s $CWD/.tmux.conf ~/.tmux.conf
+if ask "Do you want to install .bashrc?" ; then
+    ln -s $(realpath .bashrc) ~/.bashrc
+fi
